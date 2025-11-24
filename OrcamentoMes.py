d71d84e6cde1688756d39fc0463c7e574683702d
Renan Davi <renanmunhoz17@gmail.com>
2025-11-24 14:42:18 -0300

    Delete OrcamentoMes.py

--- OrcamentoMes.py
+++
@@ -1,7 +0,0 @@
-class OrcamentoMes: # representa o orcamento do mes
-
-    def __init__(self, ano, mes, definir_orcamento, definit_valor):
-        self.ano = ano
-        self.mes = mes
-        self.definir_orcamento = definir_orcamento
-        self.definir_valor = definit_valor
