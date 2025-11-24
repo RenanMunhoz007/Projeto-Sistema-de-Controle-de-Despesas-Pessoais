dbc91d1d530a2f27711bb366e8ccc7156f5a0c5f
Renan Davi <renanmunhoz17@gmail.com>
2025-11-24 14:41:39 -0300

    Delete Classes  directory

--- Classes /Alerta.py
+++
@@ -1,5 +0,0 @@
-class Alerta: # alerta gerado quando o orcamento passa
-    def __init__(self, mensagem, tipo, data):
-        self.mensagem = mensagem
-        self.tipo = tipo
-        self.data = data
