a94cf05869fe9a7c8f2f7388485cd93dc0c16046
Renan Davi <renanmunhoz17@gmail.com>
2025-11-25 18:49:37 -0300

    Update Lancamento.py

--- Classes/Lancamento.py
+++ Classes/Lancamento.py
@@ -1,14 +1,35 @@
+from Classes import Categoria 
 class Lancamento: # Classe base dos Lancamentos
-    def __init__(self, valor, categoria, data, descricao, forma_pagamento):
-        self.valor = valor
-        self.categoria = categoria
-        self.data = data
-        self.descricao = descricao
-        self.forma_pagamento = forma_pagamento
+    def __init__(self, valor, data, descricao, categoria: Categoria):
+        self._valor = valor
+        self._data = data
+        self._descricao = descricao
+        self._categoria = categoria
 
-class Receita(Lancamento): # Transacao do tipo receita 
-    pass
+    @property
+    def valor(self):
+        return self._valor
 
-class Despesa(Lancamento): # Transacao tipo despesa
+    @property
+    def data(self):
+        return self._data
 
-    pass
+    @property
+    def descricao(self):
+        return self._descricao
+
+    @property
+    def categoria(self):
+        return self._categoria
+
+    def __str__(self):
+        return f"Lançamento({self.valor}, {self.data}, '{self.descricao}')"
+
+    def __eq__(self, other):
+        if not isinstance(other, Lancamento):
+            return False
+        return (
+            self.valor == other.valor and
+            self.data == other.data and
+            self.descricao == other.descricao
+        )
