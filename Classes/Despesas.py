0f0e4ff066727a15ff44404eb2ffe90ea03cd362
Renan Davi <renanmunhoz17@gmail.com>
2025-11-25 18:51:57 -0300

    Update Despesas.py

--- Classes/Despesas.py
+++ Classes/Despesas.py
@@ -1,1 +1,4 @@
-
+from Classes import Lancamento 
+class Despesa(Lancamento): # Lancamento tipo despesa
+    def __str__(self):
+        return f"Despesa({self.valor}, {self.data}, categoria={self.categoria.nome})"
